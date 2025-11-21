e650b9b23c731dbf5bb854e299a295307f3a83c2
kimhg <kimhg93@gmail.com>
2025-11-21 10:32:48 +0900

    allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,19 @@ export default defineConfig({
   plugins: [vue()],
   server: {
     host: '0.0.0.0',
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    strictPort: false,
+    hmr: {
+      clientPort: process.env.PORT || 3000
+    }
   },
   preview: {
     host: '0.0.0.0',
-    port: process.env.PORT || 3000
+    port: process.env.PORT || 3000,
+    strictPort: false,
+    allowedHosts: [
+      'hakathon-demo.onrender.com',
+      '.onrender.com'
+    ]
   }
 })
